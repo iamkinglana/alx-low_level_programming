f1f588201162d529d20aeaf0e9c8aa0fcbc27470
Enock Kiprotich <ngetichenock32@gmail.com>
2022-07-20 10:31:22 -0700

    Hot Fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,30 @@
 #include "main.h"
 
 /**
- * factorial - factorial n
- * @n: integer arams
- * Return: recursion
+ *_sqrt_recursion - main funct
+ * @n: int n
+ * Return: int
  */
 
-int factorial(int n)
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+
+int _sqrt(int n, int i)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	if (n == 1)
-	{
-		return (1);
-	}
-	return (n * factorial(n - 1));
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
